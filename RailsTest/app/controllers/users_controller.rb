f2bbd8cab22b643aed6b640d3d80586f7f95d8be
James Stonehill <jamesstonehill@gmail.com>
2017-01-28 12:26:43 +0000

    you can now update a users details

--- RailsTest/app/controllers/users_controller.rb
+++ RailsTest/app/controllers/users_controller.rb
@@ -5,7 +5,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(name_and_favorite_game_params)
+      flash[:notice] = "Information successfully updated for #{@user.name || @user.id }"
+      redirect_to(users_path)
+    else
+      flash[:error] = "Somethin went wrong with updating user #{@user.id}'s details"
+      render('edit')
+    end
+  end
+
+  private
+
+  def name_and_favorite_game_params
+      params.require(:user).permit(:name, :favorite_game)
   end
 
 end
--- RailsTest/config/routes.rb
+++ RailsTest/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root "users#index"
 
-  resources :users, :only => [:index, :edit]
+  resources :users, :only => [:index, :edit, :update]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
