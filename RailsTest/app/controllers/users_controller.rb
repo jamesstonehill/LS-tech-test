832431f54621009450d1de72aedb489fb6808ec9
James Stonehill <jamesstonehill@gmail.com>
2017-01-28 23:29:24 +0000

    now appending data driven elements to page

--- RailsTest/app/controllers/users_controller.rb
+++ RailsTest/app/controllers/users_controller.rb
@@ -29,7 +29,11 @@ class UsersController < ApplicationController
   def format_data_to_json
     users = User.all
 
-    data = {}
+    # [{0 => {"day" => day, "count" => 3}}, {0 => {"day" => day, "count" => 3}}]
+    # [{"date"=> date, "count" => 1},{"date"=> date, "count" => 1}]}
+    data = []
+
+    created_at_days = []
 
     users.each do |user|
       # set all date time objects to middle of day so they all users who signed
@@ -37,17 +41,19 @@ class UsersController < ApplicationController
 
       day = user.created_at.middle_of_day
 
-      if data[day].nil?
-        data[day] = 1
-      else
-        data[day] = data[day] += 1
-      end
+      created_at_days << day
+    end
+
+    unique_days = created_at_days.uniq
+
+    unique_days.each do |day|
+      data.append({"date" => day, "count" => created_at_days.count(day) })
     end
 
     data.to_json
   end
 
   def name_and_favorite_game_params
-      params.require(:user).permit(:name, :favorite_game)
+    params.require(:user).permit(:name, :favorite_game)
   end
 end
